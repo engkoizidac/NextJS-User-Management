50c9fd518a78a5c3f41e5a09bb6b0a4e62a4d55b
LESTER JOHN PAUL CADIZ <cadizlester@gmail.com>
2026-07-01 13:45:23 +0800

    Update Logo on Login Form
    Update Logo on Login Form

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,14 +1,26 @@
+import Image from "next/image";
 import { LoginForm } from "./login-form";
 
 export default function Login() {
   return (
-    <div className=" flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+    <div className="flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
       <div className="max-w-md w-full space-y-4">
-        <div className="text-center">
-          <h1 className="text-2xl md:text-3xl font-semibold">
-            User<span className="text-blue-500">Auth</span>
+        <div className="flex justify-center">
+          <h1 className="flex items-center text-3xl font-extrabold tracking-tight">
+            <span>PETR</span>
+
+            <Image
+              src="/logo.png"
+              alt="PetroFlow Logo"
+              width={34}
+              height={34}
+              className="-ml-0.5 -mr-0.5"
+            />
+
+            <span className="text-blue-500">FLOW</span>
           </h1>
         </div>
+
         <LoginForm />
       </div>
     </div>
